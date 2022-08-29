a34946f66efb31c1f59cd421f0b24313177bc9ee
MirsonCamadro <kztempus@gmail.com>
2022-08-29 10:49:47 -0400

    added Functions.jsx and MathOperations.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import Button from './components/Button'
+import Functions from './components/Functions'
+import MathOperations from './components/MathOperations'
 import Result from './components/Result'
 import './App.css'
 
 // Función Flecha o Arrow Function
 const App = () => {
+
+    const clickHandlerFunction = text => {
+        console.log(text)
+    }
     // Lo que ejecuta la función
     console.log("Renderización de App")
     return (
         <main className='react-calculator'>
-            <Result value={"1"}/>
+            <Result />
             <div className="numbers">
-                <button>1</button>
+                <Button text={"1"} clickHandler={clickHandlerFunction}/>
                 <button>2</button>
                 <button>3</button>
                 <button>4</button>
@@ -21,17 +28,14 @@ const App = () => {
                 <button>9</button>
                 <button>0</button>
             </div>
-            <div className="functions">
-                <button>Clear</button>
-                <button>Remove</button>
-            </div>
-            <div className="math-operations">
-                <button>+</button>
-                <button>-</button>
-                <button>*</button>
-                <button>/</button>
-                <button>=</button>
-            </div>
+            <Functions 
+                onContentClear={() => console.log("clear")}
+                onDelete={() => console.log("delete")}
+            />
+            <MathOperations 
+                onClickOperation={operation => console.log(operation)}
+                onClickEqual={equal => console.log(equal)}
+            /> 
         </main>
     )
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Result = ({ value }) => {
-    return (
-        <div className="result">
-            { value }
-        </div>
-    )
-};
+const Result = ({ value }) => (
+    <div className="result">
+        { value }
+    </div>
+)
+
 
 // Validacion del tipo de dato
 Result.propTypes = {
     value: PropTypes.string.isRequired
+}
+
+//valor por defecto
+
+Result.defaultProps = {
+    value: "0"
 }
 
 export default Result;

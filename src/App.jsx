96a9160ac27bba322d81db7a4a305afd2421cd63
MirsonCamadro <kztempus@gmail.com>
2022-08-30 12:34:22 -0400

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
+/* eslint no-eval: 0 */
 import React, { useState } from 'react'
+import words from 'lodash.words'
 import Functions from './components/Functions'
 import Numbers from './components/Numbers'
 import MathOperations from './components/MathOperations'
 import Result from './components/Result'
 import './App.css'
+import { buildTimeValue } from '@testing-library/user-event/dist/utils'
 
 // Función Flecha o Arrow Function
 const App = () => {
     // Array Destructuring
     const [stack, setStack] = useState("")
+
+    const items = words(stack, /[^-^⁺^*^/]+/g)
     // Lo que ejecuta la función
+    const value = items.length > 0 ? items[items.length-1] : 0
+    
     console.log("Renderización de App")
     return (
         <main className='react-calculator'>
-            <Result value={stack}/>
+            <Result value={value}/>
             <Numbers onClickNumber={number => {
                 setStack(`${stack}${number}`)
             }}/>
